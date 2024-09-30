1ab218afb57e0f5d5394712c8ae10316e3ab663d
JustOnesAndZeros <benwray2005@gmail.com>
2024-09-30 14:57:32 +0100

    added blueprints

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
 ﻿from flask import Flask
 
 app = Flask(__name__)
+
+# IMPORT BLUEPRINTS
+from accounts.views import accounts_bp
+from posts.views import posts_bp
+from security.views import security_bp
+
+# REGISTER BLUEPRINTS
+app.register_blueprint(accounts_bp)
+app.register_blueprint(posts_bp)
+app.register_blueprint(security_bp)
