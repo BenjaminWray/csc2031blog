2de02b3a8aa53096761398465e7f253b6dac5120
JustOnesAndZeros <benwray2005@gmail.com>
2024-09-30 14:04:07 +0100

    added registration.html

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home/index.html')
+
+
+@app.route('/registration')
+def registration():
+    return render_template('accounts/registration.html')
 
 
 if __name__ == '__main__':
