82134acf4574a9ba48ad9a4dff79b70b39943d37
JustOnesAndZeros <benwray2005@gmail.com>
2024-09-30 14:00:32 +0100

    added index.html

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 ﻿from flask import Flask
-
-app = Flask(__name__)
+from config import app
+from flask import render_template
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
---
+++ config.py
@@ -0,0 +1,3 @@
+﻿from flask import Flask
+
+app = Flask(__name__)
