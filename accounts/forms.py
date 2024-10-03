35171787c481cca8d0bd7aa677febb6c7059f162
JustOnesAndZeros <benwray2005@gmail.com>
2024-10-03 15:28:59 +0100

    added registration form

---
+++ accounts/forms.py
@@ -0,0 +1,12 @@
+﻿from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo
+
+class RegistrationForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired()])
+    firstname = StringField('first name', validators=[DataRequired()])
+    lastname = StringField('last name', validators=[DataRequired()])
+    phone = StringField('phone number', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    confirm_password = PasswordField('confirm password', validators=[DataRequired(), EqualTo('password', message='Both password fields must be equal!')])
+    submit = SubmitField('Register')
